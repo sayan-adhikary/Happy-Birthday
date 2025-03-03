03db4a4b9f2677f79b56d896998880927740819a
SAYAN ADHIKARY <103528635+sayan-adhikary@users.noreply.github.com>
2025-03-03 11:41:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function changeMessage() {
+    const messageElement = document.getElementById("message");
+    const nameElement = document.getElementById("name");
+  
+    const messages = [
+      "Wishing you a day filled with love and joy! 🎂",
+      "Have an amazing year ahead! 🎁",
+      "Here's to a fabulous year of celebrations! 🎉",
+      "May your birthday be as beautiful as you are! 💖",
+      "Loves reading mystery novels at sex time 📚🔍",
+      "I know you are enjoys playing with dick every weekend with all friends ⚽️👟",
+      "Everyone knows u are learning to play the dick as piano 🎹🎶",
+      "all know U planning a surprise party for licking best friend's dick 🎉🎁",
+      "U have a green thumb and grows beautiful flowers in your ass 🌸🌿",
+      "Traveled to Japan last summer and had an amazing experience with some other dick ✈️",
+      "U are a fantastic painter and often participates to draw the ass of yours 🎨🖌️",
+      "Has a knack for solving puzzles and always challenges your friends that you have more experience with some other dick 🧩🤔",
+      "Loves hiking and often goes on weekend trips to to lick dicks at the mountains 🏞️🥾",
+      "Enjoys cooking and trying new recipes from different motherFucker who fuck you 🍳🍴",
+    ];
+  
+    const names = [
+      "Alice",
+      "Boobs",
+      "Remi Lacroix",
+      "Diana",
+      "Eva Elfie",
+      "Dani Daniels",
+      "Mia Khalifa",
+      "Mia Malkova",
+      "Ella",
+      "Liam",
+      "Valentina Nipples",
+      "Eva Elfie",
+      "Sophie",
+      "James"
+    ];
+  
+    // Randomly change the message and name
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+    const randomName = names[Math.floor(Math.random() * names.length)];
+  
+    messageElement.textContent = randomMessage;
+    nameElement.textContent = randomName;
+  }
+  
